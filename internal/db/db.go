d3265a46c44fffb8397a3ea9d71e402ea6184706
oshribelay <oshri.belay@gmail.com>
2025-06-19 13:05:41 +0300

    feat: added helm chart and moved db uri to env var

--- internal/db/db.go
+++ internal/db/db.go
@@ -7,31 +7,38 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"os"
 	"time"
 )
 
 func InsertClusterInfoToMongo(logger logr.Logger, clusterInfo v1alpha1.ClusterInfo) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-
+	mongoURI, ok := os.LookupEnv("MONGO_URI")
+	if !ok {
+		logger.Error(nil, "MONGO_URI environment variable not set")
+		return
+	}
 	client, err := mongo.Connect(
 		ctx,
-		options.Client().ApplyURI("<mongo-uri>"),
+		options.Client().ApplyURI(mongoURI),
 	)
 	if err != nil {
 		logger.Error(err, "Failed to connect to MongoDB")
 		return
 	}
 	defer client.Disconnect(ctx)
-
-	collection := client.Database("axiom").Collection("clusterInfo")
-	filter := bson.M{"clusterID": clusterInfo.Status.ClusterID}
-	update := bson.M{"$set": clusterInfo}
-	opts := options.Update().SetUpsert(true)
-	_, err = collection.UpdateOne(ctx, filter, update, opts)
-	if err != nil {
-		logger.Error(err, "Failed to insert cluster info to MongoDB")
-		return
+	if clusterInfo.Status.ClusterID != "" {
+		collection := client.Database("axiom").Collection("clusterInfo")
+		filter := bson.M{"clusterID": clusterInfo.Status.ClusterID}
+		update := bson.M{"$set": clusterInfo.Status}
+		opts := options.Update().SetUpsert(true)
+		_, err = collection.UpdateOne(ctx, filter, update, opts)
+		if err != nil {
+			logger.Error(err, "Failed to insert cluster info to MongoDB")
+			return
+		}
+		logger.Info("Inserted cluster info to MongoDB")
 	}
-	logger.Info("Inserted cluster info to MongoDB")
+	return
 }
